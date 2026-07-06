393ef04efd2e0dcc41f8c7b26741d7a9ebad92d4
Jaydeep1312-2007 <darjijaydeep1312@gmail.com>
2026-07-06 18:44:43 +0530

    Completed Print Function in Python

---
+++ Print Function/Simple_Print_Function.py
@@ -0,0 +1,15 @@
+print("Simple Print Function")
+
+print("Print a multiple data types in single function")
+
+print("Jaydeep",34,"Captain America",34.5,True)
+
+print("NO need of \n for a different print function")
+
+#seperate by a space to comma
+
+print("Jaydeep",34,"Captain America",34.5,True,sep=',')
+
+print("Name","Jaydeep",sep="->", end=",")
+
+print("Age","19",sep="->")
